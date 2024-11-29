1a3c37d17b81e2c2461846f9dc67f5d6ee97889c
Wimwipa780 <wimwipha4746@gmail.com>
2024-11-29 16:22:57 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
+int num;
+int even = 0;
+int odd = 0;
 
 int main(){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    while(num!=0){
+    if(num%2==0){
+    even++;
+    } 
+    else{
+    odd++;
+    }
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    }
+    cout << "#Even numbers = " << even << endl; 
+    cout << "#Odd numbers = " << odd << endl;
+    
     return 0;
+    
 }
